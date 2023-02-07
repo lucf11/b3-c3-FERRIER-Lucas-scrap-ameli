0db4f6c2bf7841a6e580172478e395ea2fde0ba9
lucf11 <lucas11ferrier@gmail.com>
2023-02-07 14:43:11 +0100

    premiere partie : récupérer page html

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+url = "http://annuairesante.ameli.fr/"
 
+header = {
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+requete = requests.Session();
+page = requete.get(url)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+soup = BeautifulSoup(page.text, 'html.parser')
+print(soup)
