ffed4e1caacd849d3ec548d7e1127da856453f6e
lucf11 <lucas11ferrier@gmail.com>
2023-02-07 15:31:56 +0100

    correction headers

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 url = "http://annuairesante.ameli.fr/"
 
 header = {
-    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 }
 
 requete = requests.Session()
